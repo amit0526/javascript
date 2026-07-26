8c4690742e286e966c1e091575a2ed1c7e40c40d
Amit Anand <amit.anand0526@gmail.com>
2026-07-26 22:19:04 +0530

    Refactor spacing in even-odd function and uncomment output

--- 40-logic-building/practice2/even-odd.js
+++ 40-logic-building/practice2/even-odd.js
@@ -4,10 +4,11 @@ const prompt = require('prompt-sync')();
 const n = Number(prompt("Enter your number: "));
 
 function check(n) {
-  if (n%2===0) {
+  if (n % 2===0 ) {
     return `${n} is even number`;
+    
   } else {
     return `${n} is odd number`;
   }
 }
-// console.log(check(n));
+console.log(check(n));
