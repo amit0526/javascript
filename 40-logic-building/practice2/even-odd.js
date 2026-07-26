d3a1447ea129a02565965c99acc7c047a7816b31
Amit Anand <amit.anand0526@gmail.com>
2026-07-26 22:32:02 +0530

    Fix logic in check function and update console output for clarity

--- 40-logic-building/practice2/even-odd.js
+++ 40-logic-building/practice2/even-odd.js
@@ -1,14 +1,23 @@
 // 4.Write a function that tells if a given number is even or odd.
 
 const prompt = require('prompt-sync')();
-const n = Number(prompt("Enter your number: "));
+// const n = Number(prompt("Enter your number: "));
 
 function check(n) {
-  if (n % 2===0 ) {
+  if (n % 2 ) {
     return `${n} is even number`;
-    
   } else {
     return `${n} is odd number`;
   }
 }
-console.log(check(n));
+// console.log(check(n));
+
+// Method 2
+
+const a = Number(prompt("Enter your number: "));
+
+function checkNumber(a) {
+  return a % 2 === 0 ? "Even": "Odd";
+}
+
+console.log(checkNumber(a));
