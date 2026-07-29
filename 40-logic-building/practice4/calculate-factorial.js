08050bccb6e37b09558e000aab42e71a7d1e7b20
Amit Anand <amit.anand0526@gmail.com>
2026-07-29 21:14:20 +0530

    Refactor factorial calculation function for improved error handling and readability

--- 40-logic-building/practice4/calculate-factorial.js
+++ 40-logic-building/practice4/calculate-factorial.js
@@ -1,17 +1,34 @@
 // Q7.Write a function that calculate factorial of a given number.
 
-
 const prompt = require('prompt-sync')();
 let targetNum = Number(prompt("Enter your number: "));
 
+function calculate (targetNum) {
 
-function calculate(targetNum) {
+  if (isNaN(targetNum)) {
+    throw new Error('Enter a vaild number!');
+  }
+
+ if (targetNum < 0) {
+   throw new Error("Enter a non-negative number!");
+   
+  }
+  
   let fact = 1;
+
   for (let i = 1; i <= targetNum; i++) {
-    fact = fact * i;
-}
+    fact *= i;
+    
+  }
   return fact;
 }
-const check = calculate(targetNum);
-console.log(check);
 
+try {
+  const result = calculate(targetNum);
+  console.log(`Factorial of ${targetNum} is ${result}`);
+
+ } catch (error) {
+  console.log(error.message);
+ }
+
+  
