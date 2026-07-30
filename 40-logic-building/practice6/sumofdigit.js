19cb1a6852062fa7d5075e39b06dddd6048b9eea
Amit Anand <amit.anand0526@gmail.com>
2026-07-30 21:50:34 +0530

    Comment out user input and console log statements for clarity in sum of digits calculation

--- 40-logic-building/practice6/sumofdigit.js
+++ 40-logic-building/practice6/sumofdigit.js
@@ -1,7 +1,7 @@
 // Q8.Write a function that calculates and print the sum of the digits of given number.
 
 const prompt = require('prompt-sync')();
-const sum = Number(prompt('Enter your number: '));
+// const sum = Number(prompt('Enter your number: '));
 function calculate(sum) {
     if (sum<1) {
       throw new Error(`Number must be above 0`);
@@ -15,4 +15,22 @@ function calculate(sum) {
   return sumofDigit;
   
  }
-console.log(calculate(sum));
+// console.log(calculate(sum));
+
+// Method 2
+
+const num = Number(prompt("Enter your number: "));
+
+function calculate2(num) {
+
+  let sumofDigit = 0;
+
+  while (num > 0) {
+    let lastDigit = num % 10;
+    num = Math.floor(num / 10);
+    console.log("last digit: ", lastDigit, 'New Number', num);
+    sumofDigit = sumofDigit + lastDigit;
+  }
+  return sumofDigit;
+}
+console.log(calculate2(num))
