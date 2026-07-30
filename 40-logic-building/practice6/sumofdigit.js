62efffe186bf202106860d1dd870e0aef840854a
Amit Anand <amit.anand0526@gmail.com>
2026-07-30 21:19:04 +0530

    Add function to calculate the sum of the digits of a given number

---
+++ 40-logic-building/practice6/sumofdigit.js
@@ -0,0 +1,18 @@
+// Q8.Write a function that calculates and print the sum of the digits of given number.
+
+const prompt = require('prompt-sync')();
+const sum = Number(prompt('Enter your number: '));
+function calculate(sum) {
+    if (sum<1) {
+      throw new Error(`Number must be above 0`);
+  }
+  const sumAsString = sum.toString();
+  const splitSumString = sumAsString.split('');
+  let sumofDigit = 0;
+  splitSumString.forEach(num => {
+    sumofDigit = sumofDigit + parseInt(num);
+  })
+  return sumofDigit;
+  
+ }
+console.log(calculate(sum));
