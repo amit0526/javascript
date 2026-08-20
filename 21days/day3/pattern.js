6af3dc1968485e8bd7621d5ffad52224e55e281d
Amit Anand <amit.anand0526@gmail.com>
2026-08-20 19:48:29 +0530

    Refactor pattern printing logic and remove user input prompts

--- 21days/day3/pattern.js
+++ 21days/day3/pattern.js
@@ -1,9 +1,22 @@
-const prompt = require('prompt-sync')();
-let n = Number(prompt("Enter a number: "))
+// 1.Print Right-angled star Triangle.
+  
+let n = 5;
 
-for (let i = n; i >= 1; i--){
+for (let i = 1; i <=n; i++) {
   let row = "";
-  for (let j = 1; j <= i; j++) {
+  for (let j = 1; j <=i; j++) {
+    row += "*";
+    }
+  // console.log(row);
+}
+
+// 2. Print invert Right-Angled triangle.
+
+let n1 = 6;
+
+for (let i = n1; i >= 1; i--){
+  let row = "";
+  for (let j = 1; j <= i; j++){
     row += "*";
   }
   console.log(row);
